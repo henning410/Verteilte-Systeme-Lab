3ae05668a472e434293d3ca7d05b2f0b7b864adb
heweit00 <heweit00@hs-esslingen.de>
2022-04-04 11:34:35 +0200

    added endpoint with parameter

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -18,6 +16,12 @@ public class DemoApplication {
 		@GetMapping("/hello")
 		String hello() {
 			return "Hello here I am";
+		}
+
+		@GetMapping("/parameter/{id}")
+		@ResponseBody
+		String testParameter(@PathVariable("id") String id) {
+			return "Parameter ID: " + id;
 		}
 	}
 
